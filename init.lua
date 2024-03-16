f9ae4db6521307d059f7bbadb5f3464bfa01562d
Asher <58609369+SowbugMori@users.noreply.github.com>
2024-03-16 12:36:51 -0500

    Moved rich presence to it's own file.

--- init.lua
+++ init.lua
@@ -225,7 +225,6 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  'andweeb/presence.nvim',
 
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically

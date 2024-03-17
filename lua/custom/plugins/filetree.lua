9b8cf04149c7b9e6f86bbce745721a485926c6cb
Asher <58609369+SowbugMori@users.noreply.github.com>
2024-03-17 01:06:31 -0500

    updaate filetree and statusline

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -7,7 +7,26 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      enable_git_status = true,
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            -- Change type
+            added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted = '✖', -- this can only be used in the git_status source
+            renamed = '󰁕', -- this can only be used in the git_status source
+            -- Status type
+            untracked = '',
+            ignored = '',
+            unstaged = '󰄱',
+            staged = '',
+            conflict = '',
+          },
+        },
+      },
+    }
     vim.cmd [[nnoremap \ :Neotree toggle<cr>]]
   end,
 }

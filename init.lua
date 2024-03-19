05790e18b842130db467583cf658976019816471
Asher <58609369+SowbugMori@users.noreply.github.com>
2024-03-19 15:07:09 -0500

    Relative line numbers

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -782,7 +782,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---- @diagnostic disable-next-line: duplicate-set-field
       -- statusline.section_location = function()
-        -- return '%2l:%-2v'
+      -- return '%2l:%-2v'
       -- end
 
       -- ... and there is more!
